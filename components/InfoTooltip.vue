c8bec88d422786daf33f053b8b7e08e3ebb58059
max <max@conquers.co>
2020-06-03 11:17:21 +0800

    chore: add test class

--- components/InfoTooltip.vue
+++ components/InfoTooltip.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg class="bi bi-info-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
+  <svg class="bi bi-info-circle ml-5px mr-5px" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
        xmlns="http://www.w3.org/2000/svg"
        v-tooltip="content"
   >
