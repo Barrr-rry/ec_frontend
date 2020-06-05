2608826c3eb19ecb8d102d95cb28b1e0298f554d
max <max@conquers.co>
2020-06-05 10:26:55 +0800

    chore: 調整footer 右邊(左邊尚未調整 NO ICON）

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,7 +5,7 @@
         <div class="container">
           <div class="row">
             <div
-              class="col-12 col-sm-12 col-md-4 text-sm-center text-md-left footer-left"
+              class="col-12 col-sm-12 col-md-6 text-sm-center text-md-left footer-left"
             >
               <div class="footer-logo">
                 <a href="/"></a>
@@ -23,35 +23,33 @@
                 <a class="" href=""><i class="fab twitter" @click="share('twitter')"></i></a>
               </div>
             </div>
-            <div class="col-md-8 foot-right">
+            <div class="col-md-6 col-sm-12 foot-right">
               <div class="row">
                 <div
                   class="col-12 col-sm-4 text-sm-center text-md-left footer-margin"
                 >
-                  <FooterQuickLink :title= "$t('ordering_information')">
+                  <FooterQuickLink title="會員中心">
                     <nuxt-link to="/delivery-instructions"
-                      >{{$t('delivery_instructions')}}
+                    >我的帳戶
                     </nuxt-link>
-                    <nuxt-link to="/terms">{{$t('company_terms')}}</nuxt-link>
-                    <nuxt-link to="/rewards">{{$t('loyalty_reward')}}</nuxt-link>
-                    <nuxt-link to="/faq">{{$t('common_problem')}}</nuxt-link>
+                    <nuxt-link to="/terms">我的最愛</nuxt-link>
+                    <nuxt-link to="/rewards">訂單查詢</nuxt-link>
                   </FooterQuickLink>
                 </div>
                 <div
                   class="col-12 col-sm-4 text-sm-center text-md-left footer-margin"
                 >
-                  <FooterQuickLink :title="$t('member_center')">
-                    <nuxt-link to="/member-centre">{{$t('my_account')}}</nuxt-link>
-                    <nuxt-link to="/wishlist">{{$t('my_favourite')}}</nuxt-link>
-                    <nuxt-link to="/ordertracking">{{$t('order_record')}}</nuxt-link>
+                  <FooterQuickLink title="訂單資訊">
+                    <nuxt-link to="/member-centre">尺寸表</nuxt-link>
+                    <nuxt-link to="/wishlist">條款與細則</nuxt-link>
+                    <nuxt-link to="/ordertracking">商業合作</nuxt-link>
                   </FooterQuickLink>
                 </div>
                 <div
                   class="col-12 col-sm-4 text-sm-center text-md-left footer-margin"
                 >
-                  <FooterQuickLink :title="$t('other_information')">
-                    <nuxt-link to="/privacy-statement">{{$t('privacy_statement')}}</nuxt-link>
-                    <nuxt-link to="/coupon">{{$t('coupon')}}</nuxt-link>
+                  <FooterQuickLink title="訂購資訊">
+                    <nuxt-link to="/privacy-statement">配送與換貨說明</nuxt-link>
                   </FooterQuickLink>
                 </div>
               </div>
@@ -70,7 +68,7 @@
             <div
               class="right col-12 col-md-5 d-flex justify-content-center justify-content-md-end"
             >
-              <img class="payment-method" src="/images/webs/visa.svg" alt="" />
+              <img class="payment-method" src="/images/webs/visa.svg" alt=""/>
             </div>
           </div>
         </div>
@@ -79,40 +77,40 @@
   </div>
 </template>
 <script>
-import FooterQuickLink from '@/components/FooterQuicklink'
+  import FooterQuickLink from '@/components/FooterQuicklink'
 
-export default {
-  components: {
-    FooterQuickLink
-  },
-  data() {
-    return {}
-  },
-  methods: {
-    share(social) {
-      const webTitle = `EZGO`,
-      webUrl = location.href
-      window.open('https://www.addtoany.com/add_to/' + social + '?linkurl=' + webUrl + '&amp;linkname=' + encodeURI(webTitle))
+  export default {
+    components: {
+      FooterQuickLink
     },
+    data() {
+      return {}
+    },
+    methods: {
+      share(social) {
+        const webTitle = `EZGO`,
+          webUrl = location.href
+        window.open('https://www.addtoany.com/add_to/' + social + '?linkurl=' + webUrl + '&amp;linkname=' + encodeURI(webTitle))
+      },
+    }
   }
-}
 </script>
 
 <style>
-ul.list-language li {
-  white-space: nowrap;
-}
+  ul.list-language li {
+    white-space: nowrap;
+  }
 
-.bg-green {
-  background: #0b1d37;
-}
+  .bg-green {
+    background: #0b1d37;
+  }
 
-.inner-padding {
-  padding: 50px 0;
-}
+  .inner-padding {
+    padding: 50px 0;
+  }
 
-footer .footer-contact p,
-footer .footer-quicklink a {
-  color: #fff;
-}
+  footer .footer-contact p,
+  footer .footer-quicklink a {
+    color: #fff;
+  }
 </style>
