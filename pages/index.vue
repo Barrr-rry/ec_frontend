2276fa292c438e39f65921f660b0ec2292256bbd
max <max@conquers.co>
2020-06-05 10:03:45 +0800

    chore: search 調整(未完成） 最後再一次調整

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,57 @@
   <div id="main">
     <!-- End header-->
     <slider></slider>
+    <div class="navigation-filter">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-4 col-lg-4 col-xl-3 order-2 order-md-1">
+            <div class="department-menu_block">
+              <div
+                class="department-menu d-flex justify-content-between align-items-center white-color"
+              >
+                <i class="fas fa-bars"></i>{{$t('chose_buy')}}<span
+              ><i class="arrow_carrot-down"></i
+              ></span>
+              </div>
+              <div v-show="init_slideup" class="department-dropdown-menu">
+                <ul>
+                  <li v-for="tag of tags"
+                      :id="tag.id"
+                  >
+                    <nuxt-link :to="`/products?t=${tag.id}`">{{tag.name}}</nuxt-link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-8 col-lg-8 col-xl-9 order-1 order-md-2">
+            <div class="website-search">
+              <div class="row no-gutters">
+                <div class="col-9 col-md-8 col-lg-9 col-xl-10">
+                  <div class="search-input">
+                    <input
+                      :placeholder="$t('please_input_brand_or_prodcut_name')"
+                      class="no-round-input no-border"
+                      type="text"
+                      v-model="search_input"
+                      @keyup.enter="search"
+                      :maxlength="40"
+                    />
+                  </div>
+                </div>
+                <div class="col-3 col-md-4 col-lg-3 col-xl-2">
+                  <button class="no-round-btn" style="height: 50px"
+                          @click="search"
+                  >
+                    {{$t('search')}}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- End slider-->
     <div class="feature-products mb-40px mt-40px mt-80px-md">
       <div class="container">
@@ -207,7 +258,16 @@
     fetch(ctx) {
       return fetchReturn(ctx, [ctx.store.dispatch('banner/getList')])
     },
+    data() {
+      return {
+        init_slideup: false,
+        search_input: '',
+      }
+    },
     computed: {
+      ...mapState('tag', {
+        tags: (state) => state.items
+      }),
       tab_keys() {
         let keys = []
 
@@ -250,6 +310,9 @@
       })
     },
     methods: {
+      search() {
+        this.$router.push(`/products?k=${this.search_input.trim()}`)
+      },
       nextSlick() {
         $('.slick-next').trigger('click')
       }
