a7798e25d59090ca04b193ac8f88d45188bfa67b
max <max@conquers.co>
2020-06-05 08:33:27 +0800

    test: add for test

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,20 +15,6 @@ export default {
   vue: {
     devtool: true,
   },
-  // 開始加入GA code
-  'google-gtag': {
-    id: 'UA-145327369-3', // 必填，請填寫剛申請到的追蹤碼ID
-    config: {
-      // 這裡是填寫對gtag的需求選項」
-      anonymize_ip: true,
-      send_page_view: false, // 避免頁面刷新時後的重複追蹤
-      linker: { // 跨域追蹤，追蹤兩個相關但不同網域的頁面
-        domains: ['ezgo-buy.com', 'li1593-188.members.linode.com']
-      }
-    },
-    debug: true, // 允許在開發中進行追蹤
-    disableAutoPageTrack: false // 關閉追蹤每個頁面路由
-  },
   /*
    ** Headers of the page
    */
