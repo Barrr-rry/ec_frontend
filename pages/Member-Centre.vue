76c80b4c22f8d5ec85468e6193650abb47227a37
barry <barry@conquers.co>
2020-08-03 15:36:11 +0800

    fix: #我的帳戶：手機版忠誠獎勵跑版
    fix #45

--- pages/Member-Centre.vue
+++ pages/Member-Centre.vue
@@ -59,24 +59,51 @@
                 </div>
                 <div class="d-flex mt-3">
                   <!--忠誠獎勵-->
-                  <card-border :title="$t('loyalty_reward_m')">
-                    <div class="d-flex mb-15px align-items-center">
-                      目前的獎勵金
-                      <InfoTooltip
-                        :content="`下次消費可折抵 ${myself.record_info.record.total_point} 元新台幣`"
-                      />
-                      : {{myself.record_info.record.total_point}} 元
-                      <span v-if="myself.record_info.record.year">（最近一筆 {{myself.record_info.record.last_point}}
-                      元獎金將於 {{myself.record_info.record.year}} 年 {{myself.record_info.record.month}} 月 {{myself.record_info.record.day}} 日 到期）</span>
+                  <card-border :title="$t('loyalty_reward_m')" class="color-primary">
+                    <div class="mb-15px">
+                      <div class="row">
+                        <div class="col-12 col-md-6">
+                          <div class="point-group d-flex">
+                            <div class="point-group--title">
+                              <span>目前的獎勵金</span>
+                              <InfoTooltip
+                                :content="`下次消費可折抵 ${myself.record_info.record.total_point} 元新台幣`"
+                              />:
+                            </div>
+                            <div class="point-group--info">
+                              <span class="point-group--block">
+                                {{myself.record_info.record.total_point}}
+                              </span>
+                              元
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-12 col-md-6"><span v-if="myself.record_info.record.year" style="font-size:14px; float:right ">（最近一筆 {{myself.record_info.record.last_point}}
+                      元獎金將於 {{myself.record_info.record.year}} 年 {{myself.record_info.record.month}} 月 {{myself.record_info.record.day}} 日 到期）</span></div>
+                      </div>
                     </div>
-                    <div class="d-flex mb-15px red-color align-items-center">
-                      待生效獎勵金
-                      <InfoTooltip
-                        :content="`獎勵金生效日為消費後 ${myself.record_info.still_day} 天`"
-                      />
-                      : {{myself.record_info.record_temp.total_point}} 元
-                      <span v-if="myself.record_info.record_temp.year">（最近一筆 {{myself.record_info.record_temp.last_point}} 元獎金將於
+                    <div class="mb-15px red-color">
+                      <div class="row">
+                        <div class="col-12 col-lg-5">
+                          <div class="point-group d-flex">
+                            <div class="point-group--title">
+                              <span>待生效獎勵金</span>
+                              <InfoTooltip
+                                :content="`獎勵金生效日為消費後 ${myself.record_info.still_day} 天`"
+                              />：
+                            </div>
+                            <div class="point-group--info">
+                              <span class="point-group--block">
+                                {{myself.record_info.record_temp.total_point}}
+                              </span>元
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-12 col-lg-7">
+                          <span v-if="myself.record_info.record_temp.year" style="font-size:14px; float:right ">（最近一筆 {{myself.record_info.record_temp.last_point}} 元獎金將於
                       {{myself.record_info.record_temp.year}} 年 {{myself.record_info.record_temp.month}} 月 {{myself.record_info.record_temp.day}} 日 生效）</span>
+                        </div>
+                      </div>
                     </div>
                     <!--忠誠獎勵 a link-->
                     <div class="mb-15px d-flex justify-content-end">
